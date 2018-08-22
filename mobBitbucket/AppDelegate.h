e2307a0b26ff998f24cdc5d750c2e355c55c5ec3
Baran Berk Kosker <kberkbaran@gmail.com>
2018-08-22 10:54:03 +0300

    Initial Commit

---
+++ mobBitbucket/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mobBitbucket
+//
+//  Created by Baran Berk Kosker on 22.08.2018.
+//  Copyright © 2018 Mobiversite. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ mobBitbucket/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  mobBitbucket
+//
+//  Created by Baran Berk Kosker on 22.08.2018.
+//  Copyright © 2018 Mobiversite. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
